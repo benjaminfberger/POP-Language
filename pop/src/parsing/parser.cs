dbc0ce66c3a3d8d12cd9d17a410c8c2f44ee2114
dr-dinnerbone <benjaminberger2008@gmail.com>
2026-09-19 20:07:16 +0100

    Start writing predictive recursive descent parser

--- pop/src/parsing/parser.cs
+++ pop/src/parsing/parser.cs
@@ -1,6 +1,23 @@
-﻿namespace src.parsing
+﻿using src.parsing.ast;
+
+namespace src.parsing
 {
     public class parser
     {
+        private List<token> tokens;
+        private List<iProgramNode> ast;
+        private int pointer;
+        public parser(List<token> tokens)
+        {
+            ast = new List<iProgramNode>();
+            this.tokens = tokens;
+        }
+        private token peek() => tokens[pointer + 1];
+        private void consume() => pointer++;
+        public List<iProgramNode> parse()
+        {
+            pointer = 0;
+            throw new NotImplementedException();
+        }
     }
 }
