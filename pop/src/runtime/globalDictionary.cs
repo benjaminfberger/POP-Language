a6e19a7b22a35b3735baff208f7c534f31c361d7
25bf3912 <25bf3912@barton.ac.uk>
2026-09-23 11:35:56 +0100

    Start work on runtime

---
+++ pop/src/runtime/globalDictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.runtime
+{
+    public static class globalDictionary
+    {
+        public static List<programInstance> programs = new();
+    }
+}
---
+++ pop/src/runtime/programInstance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.runtime
+{
+    public class programInstance
+    {
+        public readonly string identity;
+        public programInstance(string identity, string[] args)
+        {
+            this.identity = identity;
+            init();
+        }
+        private void init()
+        {
+
+        }
+    }
+}
